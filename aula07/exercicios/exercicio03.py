d6b680ddfe0c7ee32a19ad9f5eb96bb33f37e61f
Guilherme Macedo <130194662+Yguilhermemacedo@users.noreply.github.com>
2024-04-09 19:44:47 -0300

    exercicio de uma calculadora em python

--- aula07/exercicios/exercicio03.py
+++ aula07/exercicios/exercicio03.py
@@ -3,13 +3,13 @@ altura = float(input('Informe sua altura: '))
 imc = (peso / (altura ** 2))
 
 if imc < 20:
-    print("Você esta abaixo do peso.")
+    print("%.2f você esta abaixo do peso." %)
 elif imc >= 20 and imc <25:
-    print("Peso ideal.")
+    print("%.2f você está no peso ideal." %)
 elif imc >= 25 and imc < 30:
-    print("Você esta em sobrepeso.")
+    print("%.2f você esta em sobrepeso." %)
 elif imc >= 30 and imc < 40:
-    print("Você esta obeso.")
+    print("%.2f você esta obeso." %)
 else:
     print("Obeso Mórbido")
 
