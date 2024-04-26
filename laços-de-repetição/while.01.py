32105d0931cb9108c8ffb4316ac10c07bdffc69c
Guilherme Macedo <gleo31294@gmail.com>
2024-04-26 11:29:51 -0300

    .

--- laços-de-repetição/while.01.py
+++ laços-de-repetição/while.01.py
@@ -1,5 +1,10 @@
 r = 'S'
+nao = 0
 while r == 'S':
-    n = int(input('Digite um valor: '))
-    r = str(input('Quer continuar? [S/N]: ')).upper()
+    respN = input('Quer cagar? [S/N]: ')
+    if respN == 'S':
+        nao = nao + 1
+        r = str(input('Quer continuar? [S/N]: ')).upper()
+    elif r == 'N':
+        print(f'Voce negou {nao} vezes.')
 print('Fim')
