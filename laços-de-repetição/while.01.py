b2c1b179afeedc154fb8db94b46fb0fa1a7aedf6
Guilherme Macedo <gleo31294@gmail.com>
2024-04-26 16:34:43 -0300

    .

--- laços-de-repetição/while.01.py
+++ laços-de-repetição/while.01.py
@@ -1,14 +1,15 @@
-dividendo = int(input("Digite um numero (Base decimal) para ser convertido em Binário: "))
-numero_digitado = dividendo
-quociente = 1
-lista = []
+base_convertida = int(input('Digite qual base será convertida\n 1 - Decimal\n 2 - Hexadecimal\n 3 - Binário\n Insira a base que será convertida:'))
+base = int(input('Digite qual base você deseja\n 1 - Binário\n 2 - Decimal\n 3 - Hexadecimal\n Digite base:'))
 
-while quociente >= 1:
-  div = numero_digitado // 2
-  resto = numero_digitado % 2
-  lista.insert(0,resto)
-  quociente = dividendo // 2
-  dividendo = quociente
-
-binario = ''.join([str(item) for item in lista])
-print("O número",numero_digitado,", quando convertido em binário, vale:",binario)
+if base_convertida == 1 and base == 1:
+    dividendo = int(input("Digite um número (Base decimal) para ser convertido em Binário: "))
+    numero_digitado = dividendo
+    quociente = 1
+    lista = []
+    while quociente >= 1:
+       quociente = dividendo // 2
+       resto = dividendo % 2
+       lista.insert(0, resto)
+       dividendo = quociente
+       binario = ''.join([str(item) for item in lista])
+    print("O número", numero_digitado, " convertido em binário será:", binario)
