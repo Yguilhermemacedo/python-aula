f1609b37ea579f253b2cbfa4d8fe5c129e664193
Guilherme Macedo <130194662+Yguilhermemacedo@users.noreply.github.com>
2024-05-06 20:46:34 -0300

    ..

--- projeto.py
+++ projeto.py
@@ -4,7 +4,8 @@ base = int(input("Digite a base em que você deseja converter\n 1 [BINÁRIO]\n 2
 print('=' *100)
 
 if base == 1:
-   dividendo = numero_decimal
+   def decimal_para_binario (numero_decimal):
+   dividendo = 
    quociente = 1
    digitos = []
    while quociente >= 1:
@@ -15,7 +16,9 @@ if base == 1:
       binario = "".join(str(digito) for digito in digitos)
 print(f'O número {numero_decimal} [DECIMAL] convertido em Binário será: {binario}')
 
-if base == 2:
+
+
+elif base == 2:
     dividendo = numero_decimal
     quociente = 1
     lista = []
@@ -28,14 +31,16 @@ if base == 2:
 print(f'O número {numero_decimal} [DECIMAL] convertido em Binário será: {octal}')
     
 
-# elif base == 3:
-#     digitos = []
-#     while numero_decimal > 0:
+elif base == 3:
+    digitos = []
+    while numero_decimal > 0:
        
-#         resto = numero_decimal % 16
-#         # Converte o resto para a letra hexadecimal correspondente
-#         if resto > 9:
-#             resto += 55  # Ajusta o valor para usar letras maiúsculas
-#             resto = chr(resto)
-#         digitos.insert(0, resto)
-#         numero_decimal //= 16
+        resto = numero_decimal % 16
+        # Converte o resto para a letra hexadecimal correspondente
+        if resto > 9:
+            resto += 55  # Ajusta o valor para usar letras maiúsculas
+            resto = chr(resto)
+        digitos.insert(0, resto)
+        numero_decimal //= 16
+else:
+    print('Erro.')
