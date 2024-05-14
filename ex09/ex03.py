9fdbd6f2973880a0f4f547319cf469f027d0deb5
Guilherme Macedo <gleo31294@gmail.com>
2024-05-14 15:11:23 -0300

    .

--- ex09/ex03.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Digite um valor inteiro positivo para n: "))
-
-if n >= 0:
-    soma = 0
-    for i in range(1, n+1):
-        soma += 1/i
-    print(f"A soma S = 1 + 1/2 + 1/3 + ... + 1/{n} é: {soma:.1f}")
-else:
-    print('Numero negativo não aceito!')
